a46f24a134c4ce1b3b3694424a653f98656b3f33
Is_52Hertz <is.52hertz@gmail.com>
2025-01-28 15:01:55 +0800

    Alpha v0.1.1 : Reset players' thirst after they died.

--- src/main/java/cn/is52hertz/aLongWalkToWater/Thirst.java
+++ src/main/java/cn/is52hertz/aLongWalkToWater/Thirst.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +20,13 @@ public class Thirst implements Listener {
         if (!playerThirst.containsKey(player)) {
             playerThirst.put(player, 100);
         }
+    }
+
+    // If the player dies, reset.
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        setThirst(player, 100);
     }
 
     /**
